7c2a4f0778930a3edb90139b0d49781e474865a6
Andrew Myers <andrewmyers@andrewmyers-macbookpro2.roam.corp.google.com>
2017-06-28 06:42:44 -0700

    Reindent with 4 spaces.

--- tensorflow/java/src/main/java/org/tensorflow/examples/LabelImage.java
+++ tensorflow/java/src/main/java/org/tensorflow/examples/LabelImage.java
@@ -36,139 +36,139 @@ import static org.tensorflow.Type.*;
  * model.
  */
 public class LabelImage {
-	private static void printUsage(PrintStream s) {
-		final String url = "https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip";
-		s.println(
-				"Java program that uses a pre-trained Inception model (http://arxiv.org/abs/1512.00567)");
-		s.println("to label JPEG images.");
-		s.println("TensorFlow version: " + TensorFlow.version());
-		s.println();
-		s.println("Usage: label_image <model dir> <image file>");
-		s.println();
-		s.println("Where:");
-		s.println(
-				"<model dir> is a directory containing the unzipped contents of the inception model");
-		s.println("            (from " + url + ")");
-		s.println("<image file> is the path to a JPEG image file");
-	}
+    private static void printUsage(PrintStream s) {
+        final String url = "https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip";
+        s.println(
+                "Java program that uses a pre-trained Inception model (http://arxiv.org/abs/1512.00567)");
+        s.println("to label JPEG images.");
+        s.println("TensorFlow version: " + TensorFlow.version());
+        s.println();
+        s.println("Usage: label_image <model dir> <image file>");
+        s.println();
+        s.println("Where:");
+        s.println(
+                "<model dir> is a directory containing the unzipped contents of the inception model");
+        s.println("            (from " + url + ")");
+        s.println("<image file> is the path to a JPEG image file");
+    }
 
-	public static void main(String[] args) {
-		if (args.length != 2) {
-			printUsage(System.err);
-			System.exit(1);
-		}
-		String modelDir = args[0];
-		String imageFile = args[1];
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            printUsage(System.err);
+            System.exit(1);
+        }
+        String modelDir = args[0];
+        String imageFile = args[1];
 
-		byte[] graphDef = readAllBytesOrExit(
-				Paths.get(modelDir, "tensorflow_inception_graph.pb"));
-		List<String> labels = readAllLinesOrExit(
-				Paths.get(modelDir, "imagenet_comp_graph_label_strings.txt"));
-		byte[] imageBytes = readAllBytesOrExit(Paths.get(imageFile));
+        byte[] graphDef = readAllBytesOrExit(
+                Paths.get(modelDir, "tensorflow_inception_graph.pb"));
+        List<String> labels = readAllLinesOrExit(
+                Paths.get(modelDir, "imagenet_comp_graph_label_strings.txt"));
+        byte[] imageBytes = readAllBytesOrExit(Paths.get(imageFile));
 
-		try (Tensor<Float> image = constructAndExecuteGraphToNormalizeImage(
-				imageBytes)) {
-			float[] labelProbabilities = executeInceptionGraph(graphDef, image);
-			int bestLabelIdx = maxIndex(labelProbabilities);
-			System.out.println(String.format("BEST MATCH: %s (%.2f%% likely)",
-					labels.get(bestLabelIdx),
-					labelProbabilities[bestLabelIdx] * 100f));
-		}
-	}
+        try (Tensor<Float> image = constructAndExecuteGraphToNormalizeImage(
+                imageBytes)) {
+            float[] labelProbabilities = executeInceptionGraph(graphDef, image);
+            int bestLabelIdx = maxIndex(labelProbabilities);
+            System.out.println(String.format("BEST MATCH: %s (%.2f%% likely)",
+                    labels.get(bestLabelIdx),
+                    labelProbabilities[bestLabelIdx] * 100f));
+        }
+    }
 
-	private static Tensor<Float> constructAndExecuteGraphToNormalizeImage(
-			byte[] imageBytes) {
-		try (Graph g = new Graph()) {
+    private static Tensor<Float> constructAndExecuteGraphToNormalizeImage(
+            byte[] imageBytes) {
+        try (Graph g = new Graph()) {
 
-			// Some constants specific to the pre-trained model at:
-			// https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip
-			//
-			// - The model was trained with images scaled to 224x224 pixels.
-			// - The colors, represented as R, G, B in 1-byte each were
-			// converted to
-			// float using (value - Mean)/Scale.
-			final int H = 224;
-			final int W = 224;
-			final float mean = 117f;
-			final float scale = 1f;
+            // Some constants specific to the pre-trained model at:
+            // https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip
+            //
+            // - The model was trained with images scaled to 224x224 pixels.
+            // - The colors, represented as R, G, B in 1-byte each were
+            // converted to
+            // float using (value - Mean)/Scale.
+            final int H = 224;
+            final int W = 224;
+            final float mean = 117f;
+            final float scale = 1f;
 
-			// Since the graph is being constructed once per execution here, we
-			// can use a constant for the
-			// input image. If the graph were to be re-used for multiple input
-			// images, a placeholder would
-			// have been more appropriate.
+            // Since the graph is being constructed once per execution here, we
+            // can use a constant for the
+            // input image. If the graph were to be re-used for multiple input
+            // images, a placeholder would
+            // have been more appropriate.
 
-			Output<Float> output = new GraphBuilder(g) {
-				Output<Float> imageIdentifier(String inp) {
-					final Output<String> input = stringConstant(imageBytes);
+            Output<Float> output = new GraphBuilder(g) {
+                Output<Float> imageIdentifier(String inp) {
+                    final Output<String> input = stringConstant(imageBytes);
 
-					return div(
-							sub(resizeBilinear(
-									withScope("decode",
-											() -> expandDims(
-													cast(decodeJpeg(input, 3), FLOAT),
-													constant(0))),
-									constant(new int[] { H, W })),
-									constant(mean)),
-							constant(scale));
-				}
-			}.imageIdentifier("input");
+                    return div(
+                            sub(resizeBilinear(
+                                    withScope("decode",
+                                            () -> expandDims(
+                                                    cast(decodeJpeg(input, 3), FLOAT),
+                                                    constant(0))),
+                                    constant(new int[] { H, W })),
+                                    constant(mean)),
+                            constant(scale));
+                }
+            }.imageIdentifier("input");
 
-			try (Session s = new Session(g)) {
-				return s.runner().fetch(output.op().name()).run().get(0)
-						.expect(FLOAT);
-			}
-		}
-	}
+            try (Session s = new Session(g)) {
+                return s.runner().fetch(output.op().name()).run().get(0)
+                        .expect(FLOAT);
+            }
+        }
+    }
 
-	private static float[] executeInceptionGraph(byte[] graphDef,
-			Tensor<Float> image) {
-		try (Graph g = new Graph()) {
-			g.importGraphDef(graphDef);
-			try (Session s = new Session(g);
-					Tensor<Float> result = s.runner().feed("input", image)
-							.fetch("output").run().get(0).expect(FLOAT)) {
-				final long[] rshape = result.shape();
-				if (result.numDimensions() != 2 || rshape[0] != 1) {
-					throw new RuntimeException(String.format(
-							"Expected model to produce a [1 N] shaped tensor where N is the number of labels, instead it produced one with shape %s",
-							Arrays.toString(rshape)));
-				}
-				int nlabels = (int) rshape[1];
-				return result.copyTo(new float[1][nlabels])[0];
-			}
-		}
-	}
+    private static float[] executeInceptionGraph(byte[] graphDef,
+            Tensor<Float> image) {
+        try (Graph g = new Graph()) {
+            g.importGraphDef(graphDef);
+            try (Session s = new Session(g);
+                    Tensor<Float> result = s.runner().feed("input", image)
+                            .fetch("output").run().get(0).expect(FLOAT)) {
+                final long[] rshape = result.shape();
+                if (result.numDimensions() != 2 || rshape[0] != 1) {
+                    throw new RuntimeException(String.format(
+                            "Expected model to produce a [1 N] shaped tensor where N is the number of labels, instead it produced one with shape %s",
+                            Arrays.toString(rshape)));
+                }
+                int nlabels = (int) rshape[1];
+                return result.copyTo(new float[1][nlabels])[0];
+            }
+        }
+    }
 
-	private static int maxIndex(float[] probabilities) {
-		int best = 0;
-		for (int i = 1; i < probabilities.length; ++i) {
-			if (probabilities[i] > probabilities[best]) {
-				best = i;
-			}
-		}
-		return best;
-	}
+    private static int maxIndex(float[] probabilities) {
+        int best = 0;
+        for (int i = 1; i < probabilities.length; ++i) {
+            if (probabilities[i] > probabilities[best]) {
+                best = i;
+            }
+        }
+        return best;
+    }
 
-	private static byte[] readAllBytesOrExit(Path path) {
-		try {
-			return Files.readAllBytes(path);
-		} catch (IOException e) {
-			System.err.println(
-					"Failed to read [" + path + "]: " + e.getMessage());
-			System.exit(1);
-		}
-		return null;
-	}
+    private static byte[] readAllBytesOrExit(Path path) {
+        try {
+            return Files.readAllBytes(path);
+        } catch (IOException e) {
+            System.err.println(
+                    "Failed to read [" + path + "]: " + e.getMessage());
+            System.exit(1);
+        }
+        return null;
+    }
 
-	private static List<String> readAllLinesOrExit(Path path) {
-		try {
-			return Files.readAllLines(path, Charset.forName("UTF-8"));
-		} catch (IOException e) {
-			System.err.println(
-					"Failed to read [" + path + "]: " + e.getMessage());
-			System.exit(0);
-		}
-		return null;
-	}
+    private static List<String> readAllLinesOrExit(Path path) {
+        try {
+            return Files.readAllLines(path, Charset.forName("UTF-8"));
+        } catch (IOException e) {
+            System.err.println(
+                    "Failed to read [" + path + "]: " + e.getMessage());
+            System.exit(0);
+        }
+        return null;
+    }
 }
